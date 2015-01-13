6434c80e00e79eaa8e968c5170209a4bb2def7a4
tigerzhao <tigerzhao999@gmail.com>
2015-01-13 13:05:53 -0800

    ples work xDD

---
+++ Ch1WZ/src/Chapterhw.java
@@ -0,0 +1,14 @@
+
+public class Chapterhw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("+--------+");
+		System.out.println("| Wesley |");
+		System.out.println("+--------+");
+		//als;dkfj;alskjdflaksjd;flkasj;lfkjas;ldkfj
+	}
+}
+
+
+
---
+++ Ch2WZ/src/Chapter2WZhw.java
@@ -0,0 +1,11 @@
+
+public class Chapter2WZhw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		String MFW = "hello,nina";
+		System.out.println(MFW);
+
+	}
+
+}
